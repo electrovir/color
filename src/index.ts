279cec58291f61913c48df072635c1554a9447a1
electrovir <electrovir@users.noreply.github.com>
2026-01-04 13:31:01 +0000

    [minor] add elements

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export * from './color-class/color-formats.js';
 export * from './color-class/color-name-length.js';
 export * from './color-class/color.js';
+export * from './contrast/contrast.js';
+export * from './elements/vir-color-pair.element.js';
+export * from './elements/vir-contrast-indicator.element.js';
